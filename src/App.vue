42b09685678edbffdfc1203991bb3cf9f3c1e020
Jaime <jaimeloeuf@gmail.com>
2026-04-18 23:24:15 +0800

    Build basic chat UI.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,89 @@
 <script setup lang="ts">
-//
+import { ref, reactive } from 'vue'
+
+const currentMessageDraft = ref('')
+const messages = reactive<Array<Message>>([
+  {
+    author: 'system',
+    timestamp: new Date().toISOString(),
+    message: 'This is a system message',
+  },
+])
+
+type Message = {
+  timestamp: string
+  author: 'current-user' | 'other-user' | 'system'
+  message: string
+}
+
+async function startNewChat() {
+  //
+}
+
+/**
+ * Adds new message to the local messages array and ensure that it is not too
+ * long.
+ */
+function addNewLocalMessage(message: Message) {
+  messages.push(message)
+}
+
+async function sendNewMessage() {
+  console.log('sending new mesage', currentMessageDraft.value)
+  const message: Message = {
+    author: 'current-user',
+    timestamp: new Date().toISOString(),
+    message: currentMessageDraft.value,
+  }
+  addNewLocalMessage(message)
+  currentMessageDraft.value = ''
+}
 </script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="flex w-full flex-row justify-between p-6 md:p-12">
+    <div class="w-full">
+      <div class="flex w-full flex-row justify-between pb-4 align-middle">
+        <p class="text-2xl text-gray-500">VaporChat</p>
+        <button
+          class="cursor-pointer rounded-2xl bg-cyan-200 px-4 py-1 text-gray-600"
+          @click="startNewChat"
+        >
+          Start new Chat
+        </button>
+      </div>
+
+      <div class="flex flex-col gap-4 md:flex-row md:justify-center">
+        <div class="w-full max-w-lg">
+          <p class="pb-1 text-sm font-medium text-gray-700">Chat</p>
+          <!-- @todo Make this fixed width but scrollable within -->
+          <div
+            class="flex flex-col rounded-lg border border-gray-200 px-4 py-2 text-gray-700 shadow-sm"
+          >
+            <div
+              v-for="message in messages"
+              :key="message.timestamp"
+              class="w-max rounded-2xl border border-gray-200 px-2 py-1 text-gray-700"
+            >
+              <p>{{ message.message }}</p>
+            </div>
+          </div>
+        </div>
+
+        <div class="w-full max-w-lg">
+          <label for="message" class="block pb-1 text-sm font-medium text-gray-700">
+            Your Message
+          </label>
+          <textarea
+            id="message"
+            rows="4"
+            class="w-full resize-y rounded-lg border border-gray-200 px-4 py-2 text-gray-700 placeholder-gray-400 shadow-sm outline-none"
+            placeholder="Type your message here..."
+            v-model="currentMessageDraft"
+            @keydown.prevent.enter="sendNewMessage"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
