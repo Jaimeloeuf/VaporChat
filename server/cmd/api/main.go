dd1c8c54cf94f6e01d0dbf0f2f27d0c5d0debce5
Jaime <jaimeloeuf@gmail.com>
2026-04-20 23:02:39 +0800

    Implement basic websocket echo server.

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -2,13 +2,59 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+// Configure the Upgrader with buffer sizes
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+
+	// @todo Allow connections from any origin for testing
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Upgrade the HTTP server connection to the WebSocket protocol
+	websocketConnection, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Upgrade error:", err)
+		return
+	}
+	defer websocketConnection.Close()
+	fmt.Println("Client successfully connected!")
+
+	for {
+		// Read message from browser
+		messageType, msg, err := websocketConnection.ReadMessage()
+		if err != nil {
+			log.Println("Read error:", err)
+			break
+		}
+
+		// Print incoming message
+		fmt.Printf("Received: %s\n", msg)
+
+		// Echo message back to browser
+		err = websocketConnection.WriteMessage(messageType, msg)
+		if err != nil {
+			log.Println("Write error:", err)
+			break
+		}
+	}
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "Hello, Gopher!")
 	})
+
+	http.HandleFunc("/ws", handler)
 
 	fmt.Println("Server starting on :3000...")
 	if err := http.ListenAndServe(":3000", nil); err != nil {
