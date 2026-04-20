55c91518b852c1815d8586bd823c042803837b08
Jaime <jaimeloeuf@gmail.com>
2026-04-21 00:03:17 +0800

    Implement basic WS connection to server.

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,8 +1,47 @@
 <script setup lang="ts">
 import Chat from './Chat.vue'
 
+import { ref, onMounted } from 'vue'
+import { getWebsocketStateString } from './getWebsocketStateString'
+
+const ws = ref<WebSocket | null>(null)
+
+const wsConnectionState = ref<WebSocket['readyState'] | undefined>(undefined)
+
+// @todo Allow user to customise
+const websocketServerUrl = 'ws://localhost:3000/ws'
+
+onMounted(() => {
+  ws.value = new WebSocket(websocketServerUrl)
+  wsConnectionState.value = ws.value.readyState
+
+  ws.value.addEventListener('open', () => {
+    wsConnectionState.value = ws.value?.readyState
+  })
+  ws.value.addEventListener('close', () => {
+    wsConnectionState.value = ws.value?.readyState
+  })
+  ws.value.addEventListener('error', () => {
+    wsConnectionState.value = ws.value?.readyState
+  })
+})
+
+// onBeforeUnmount(() => {
+//   if (socket) socket.close();
+// });
+
 async function startNewChat() {
-  //
+  if (ws.value === null) {
+    console.log('no webserver connection now')
+    return
+  }
+
+  ws.value!.send('Hello Server!')
+
+  // @todo This creates a new listener everytime...
+  ws.value.addEventListener('message', function (event) {
+    console.log('Server says:', event.data)
+  })
 }
 </script>
 
@@ -10,7 +49,12 @@ async function startNewChat() {
   <div class="p-6 md:p-12">
     <div class="flex w-full flex-col">
       <div class="flex w-full flex-row justify-between pb-4 align-middle">
-        <p class="text-2xl text-gray-500">VaporChat</p>
+        <div class="flex flex-col">
+          <p class="text-2xl text-gray-500">VaporChat</p>
+          <p class="text-sm font-light text-gray-500">
+            {{ getWebsocketStateString(wsConnectionState) }}
+          </p>
+        </div>
         <button
           class="cursor-pointer rounded-2xl bg-cyan-200 px-4 py-1 text-gray-600"
           @click="startNewChat"
