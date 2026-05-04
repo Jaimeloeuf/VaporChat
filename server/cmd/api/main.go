a639e30041df6fd58327d0a4d419bd0286e140f3
Jaime <jaimeloeuf@gmail.com>
2026-05-05 00:15:27 +0800

    Refactor out websocketUpgrader function.

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -13,25 +13,6 @@ import (
 	"github.com/rs/cors"
 )
 
-var websocketUpgrader = websocket.Upgrader{
-	// @todo
-	// Buffers can be tuned based on average payload size and expected
-	// concurrent connections, to find the sweet spot between smaller buffers
-	// for lower per connection memory use and larger buffers for less syscalls
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-
-	// @todo
-	// Potentially enable pool to reuse memory across connections, and to allow
-	// for higher buffer sizes, potentially the default 4096
-	// WriteBufferPool: &sync.Pool{},
-
-	// @todo Allow connections from any origin for testing
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
-
 // Create a thread-safe map structure (Go maps are not thread-safe by default)
 type ChatStorage struct {
 	sync.RWMutex
---
+++ server/cmd/api/websocketupgrader.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var websocketUpgrader = websocket.Upgrader{
+	// @todo
+	// Buffers can be tuned based on average payload size and expected
+	// concurrent connections, to find the sweet spot between smaller buffers
+	// for lower per connection memory use and larger buffers for less syscalls
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+
+	// @todo
+	// Potentially enable pool to reuse memory across connections, and to allow
+	// for higher buffer sizes, potentially the default 4096
+	// WriteBufferPool: &sync.Pool{},
+
+	// @todo Allow connections from any origin for testing
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
