529d06f4cf34510213edf38b26ad6280dccd7fe4
Jaime <jaimeloeuf@gmail.com>
2026-05-17 00:19:33 +0800

    Add ChatUpdate type to frontend.

---
+++ webapp/src/ChatUpdate.ts
@@ -0,0 +1,33 @@
+interface BaseChatUpdate {
+  timestamp: string
+  author: string
+
+  // To override in subtypes with a string literal
+  type: string
+}
+
+export interface ChatUpdateNewStatusJoinRoom extends BaseChatUpdate {
+  type: 'status-join-room'
+}
+
+export interface ChatUpdateNewStatusLeaveRoom extends BaseChatUpdate {
+  type: 'status-leave-room'
+}
+
+export interface ChatUpdateNewMessage extends BaseChatUpdate {
+  type: 'message'
+  message: string
+}
+
+// @todo In UI show for 2s from the last time of receiving this update
+export interface ChatUpdateTyping extends BaseChatUpdate {
+  type: 'typing'
+}
+
+// @todo Support Delete message
+
+export type ChatUpdate =
+  | ChatUpdateNewStatusJoinRoom
+  | ChatUpdateNewStatusLeaveRoom
+  | ChatUpdateNewMessage
+  | ChatUpdateTyping
