451ea8c39d0a51c789b7decdc295796a1a256ae9
Jaime <jaimeloeuf@gmail.com>
2026-05-19 23:42:00 +0800

    Refactor startBackgroundChatStorageCleanupWorker out.

--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -23,30 +23,6 @@ type ChatRequest struct {
 	ChatConfig ChatConfig `json:"chatConfig"`
 }
 
-// Spawns the clean up function in the background with a goroutine
-func startBackgroundChatStorageCleanupWorker(interval time.Duration) {
-	ticker := time.NewTicker(interval)
-
-	// This goroutine runs forever in the background
-	go func() {
-		for range ticker.C {
-			log.Println("Running background ChatStorage cleanup job...")
-
-			chatStorage.Lock()
-
-			// Loop through chat rooms to delete expired ones
-			for chatID, chatRoom := range chatStorage.chatRooms {
-				if uint64(time.Now().Unix()) >= chatRoom.expiresOn {
-					log.Printf("Cleaning up empty chat room: %s", chatID)
-					delete(chatStorage.chatRooms, chatID)
-				}
-			}
-
-			chatStorage.Unlock()
-		}
-	}()
-}
-
 func main() {
 	startBackgroundChatStorageCleanupWorker(1 * time.Second)
 
