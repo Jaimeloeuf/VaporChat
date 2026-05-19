020016fa9acb0a52ae958618d60b80603c092f24
Jaime <jaimeloeuf@gmail.com>
2026-05-19 23:41:00 +0800

    Refactor broadcastMessageToChatRoom out.

---
+++ server/cmd/api/broadcastmessagetochatroom.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"errors"
+	"log"
+
+	"github.com/gorilla/websocket"
+)
+
+func broadcastMessageToChatRoom(chatID string, message string, selfWebsocketConnection *websocket.Conn) error {
+	chatStorage.RLock()
+	defer chatStorage.RUnlock()
+
+	chatRoom, chatRoomExists := chatStorage.chatRooms[chatID]
+	if !chatRoomExists {
+		return errors.New("Chat room not available")
+	}
+
+	chatMessageAsByteSlice := []byte(message)
+
+	// Broadcast message to everyone in chat room
+	for _, chatConnection := range chatRoom.websocketConnections {
+		// @todo
+		// Either we have to do this, or we have to not write it in frontend and
+		// wait for it to appear back
+		if chatConnection == selfWebsocketConnection {
+			continue
+		}
+
+		err := chatConnection.WriteMessage(websocket.TextMessage, chatMessageAsByteSlice)
+		if err != nil {
+			log.Println("Write error:", err)
+		}
+	}
+
+	return nil
+}
--- server/cmd/api/main.go
+++ server/cmd/api/main.go
@@ -18,35 +18,6 @@ var chatStorage = ChatStorage{
 	chatRooms: make(map[string]*ChatRoom),
 }
 
-func broadcastMessageToChatRoom(chatID string, message string, selfWebsocketConnection *websocket.Conn) error {
-	chatStorage.RLock()
-	defer chatStorage.RUnlock()
-
-	chatRoom, chatRoomExists := chatStorage.chatRooms[chatID]
-	if !chatRoomExists {
-		return errors.New("Chat room not available")
-	}
-
-	chatMessageAsByteSlice := []byte(message)
-
-	// Broadcast message to everyone in chat room
-	for _, chatConnection := range chatRoom.websocketConnections {
-		// @todo
-		// Either we have to do this, or we have to not write it in frontend and
-		// wait for it to appear back
-		if chatConnection == selfWebsocketConnection {
-			continue
-		}
-
-		err := chatConnection.WriteMessage(websocket.TextMessage, chatMessageAsByteSlice)
-		if err != nil {
-			log.Println("Write error:", err)
-		}
-	}
-
-	return nil
-}
-
 type ChatRequest struct {
 	UserID     string     `json:"userID"`
 	ChatConfig ChatConfig `json:"chatConfig"`
