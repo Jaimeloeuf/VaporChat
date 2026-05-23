9867643a4eb02b91efa881c5b5e0d13d122f7bf5
Jaime <jaimeloeuf@gmail.com>
2026-05-23 20:56:20 +0800

    Delete old server implementation to rebuild.

--- server/internal/server/startwebserver.go
+++ server/internal/server/startwebserver.go
@@ -7,7 +7,6 @@ import (
 	"net/http"
 
 	"github.com/Jaimeloeuf/VaporChat/internal/jsend"
-	"github.com/gorilla/websocket"
 	"github.com/rs/cors"
 )
 
@@ -19,51 +18,6 @@ func StartWebServer() {
 	serverMux.HandleFunc("/{$}", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, "Ok")
-	})
-
-	serverMux.HandleFunc("POST /api/chat/new", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-
-		var requestBody ChatRequest
-		err := json.NewDecoder(r.Body).Decode(&requestBody)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(jsend.Error("Invalid JSON format"))
-			return
-		}
-
-		newChatRoom := NewChatRoom(requestBody.ChatConfig)
-		chatStorage.AddNewChatRoom(newChatRoom)
-
-		w.WriteHeader(http.StatusCreated)
-		json.NewEncoder(w).Encode(jsend.Success(map[string]string{"chatID": newChatRoom.ID}))
-	})
-
-	serverMux.HandleFunc("/api/chat/join/{chatID}/websocket", func(w http.ResponseWriter, r *http.Request) {
-		chatID := r.PathValue("chatID")
-		log.Println("Client connecting to:", chatID)
-
-		// Upgrade HTTP server connection to WebSocket protocol
-		websocketConnection, err := websocketUpgrader.Upgrade(w, r, nil)
-		if err != nil {
-			log.Println("Websocket upgrade error:", err)
-			w.Header().Set("Content-Type", "application/json")
-			w.WriteHeader(http.StatusBadRequest)
-			json.NewEncoder(w).Encode(jsend.Error("Could not upgrade to websocket connection"))
-			return
-		}
-		log.Println("Client connected")
-
-		if err := chatStorage.saveConnection(chatID, websocketConnection); err != nil {
-			log.Printf("Connection refused for chat room ID %s: %v", chatID, err)
-			websocketConnection.WriteMessage(
-				websocket.CloseMessage,
-				websocket.FormatCloseMessage(websocket.ClosePolicyViolation, "Connection refused: chat room not available"),
-			)
-			return
-		}
-
-		go websocketLoop(websocketConnection, chatID)
 	})
 
 	serverMux.HandleFunc("/api/websocket", func(w http.ResponseWriter, r *http.Request) {
--- server/internal/server/websocketreaderloop.go
+++ server/internal/server/websocketreaderloop.go
@@ -7,29 +7,6 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-func websocketLoop(websocketConnection *websocket.Conn, chatID string) {
-	defer websocketConnection.Close()
-
-	for {
-		var chatMessage ChatMessage
-		err := websocketConnection.ReadJSON(&chatMessage)
-
-		// @todo If JSON parsing failed because of field issues, we might not want to break the connection?
-		// Exiting loop will hit the defer and clean up websocket connection
-		if err != nil {
-			log.Printf("Client disconnected or ws read message error: %v", err)
-			broadcastMessageToChatRoom(chatID, "Other user has left", websocketConnection)
-			break
-		}
-
-		// @todo Debug only, leave no logs in server
-		// Print incoming message
-		log.Printf("Received: %s\n", chatMessage.Message)
-
-		broadcastMessageToChatRoom(chatID, chatMessage.Message, websocketConnection)
-	}
-}
-
 type ChatUpdateEnvelope struct {
 	Timestamp string          `json:"timestamp"`
 	Username  string          `json:"author"`
