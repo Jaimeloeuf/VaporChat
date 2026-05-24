921a0f053831c84b8851d078499a5b46ef5d285b
Jaime <jaimeloeuf@gmail.com>
2026-05-25 00:44:49 +0800

    WIP UI refactor.

--- webapp/src/ChatStart.vue
+++ webapp/src/ChatStart.vue
@@ -39,43 +39,45 @@ const leaveChat = () => window.location.reload()
     <div>
       <LogoWithConnectionStatus />
       <div class="flex flex-col justify-center gap-8 align-middle md:flex-row">
-        <div
-          class="flex h-full w-full max-w-sm flex-col gap-4 rounded-lg border border-gray-200 px-2 py-4 shadow"
-        >
-          <div>
-            <div class="flex flex-row items-center justify-between">
-              <p>Chat ID</p>
-              <button
-                class="cursor-pointer text-sm font-light text-red-400 underline"
-                @click="joinChatID = ''"
-              >
+        <div class="h-full w-full max-w-sm">
+          <p class="pb-1 text-2xl font-light">Join chat room</p>
+          <div class="flex flex-col gap-4 rounded-lg border border-gray-200 px-2 py-4 shadow">
+            <div>
+              <div class="flex flex-row items-center justify-between">
+                <p>Chat ID</p>
+                <button
+                  class="cursor-pointer text-sm font-light text-red-400"
+                  @click="joinChatID = ''"
+                >
+                  reset
+                </button>
+              </div>
+              <input
+                v-model="joinChatID"
+                type="text"
+                class="w-full rounded border border-gray-200 p-1.5 outline-none"
+              />
+            </div>
+          </div>
+          <div class="pt-4 text-right">
+            <button
+              class="cursor-pointer rounded-2xl border border-green-400 px-4 py-1 text-green-600"
+              @click="joinChat"
+            >
+              Join Chat
+            </button>
+          </div>
+        </div>
+
+        <div class="h-full w-full max-w-sm">
+          <p class="pb-1 text-2xl font-light">New chat room</p>
+          <div class="flex flex-col gap-4 rounded-lg border border-gray-200 px-2 py-4 shadow">
+            <div class="flex flex-row justify-between border-b border-gray-200 pb-1">
+              <p>Chat Config</p>
+              <button class="cursor-pointer text-sm font-light text-red-400" @click="resetConfig">
                 reset
               </button>
             </div>
-            <input
-              v-model="joinChatID"
-              type="text"
-              class="w-full rounded border border-gray-200 p-1.5 outline-none"
-            />
-          </div>
-          <button
-            class="cursor-pointer rounded-2xl border border-green-400 px-4 py-1 text-green-600"
-            @click="joinChat"
-          >
-            Join Chat
-          </button>
-        </div>
-        <div class="h-full w-full max-w-sm">
-          <div class="flex flex-row justify-between pb-1">
-            <p>Chat Config</p>
-            <button
-              class="cursor-pointer text-sm font-light text-red-400 underline"
-              @click="resetConfig"
-            >
-              reset
-            </button>
-          </div>
-          <div class="flex flex-col gap-4 rounded-lg border border-gray-200 px-2 py-4 shadow">
             <div>
               <p>Username</p>
               <p class="text-xs">
