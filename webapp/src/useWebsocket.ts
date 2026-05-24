dbfd743e77a8fa74445224b7b335784c10ac1873
Jaime <jaimeloeuf@gmail.com>
2026-05-24 22:39:08 +0800

    Move all websocket values out to be shared globally.

--- webapp/src/useWebsocket.ts
+++ webapp/src/useWebsocket.ts
@@ -1,32 +1,32 @@
 import { ref, computed } from 'vue'
 import { getWebsocketStateString } from './getWebsocketStateString'
 
+const websocket = ref<WebSocket | null>(null)
+const websocketConnectionState = ref<WebSocket['readyState'] | undefined>(undefined)
+const websocketConnectionStateString = computed(() =>
+  getWebsocketStateString(websocketConnectionState.value),
+)
+
+const isWebsocketConnected = computed(() => websocketConnectionState.value === WebSocket.OPEN)
+
+function setupWebsocket() {
+  // @todo Move the URL into a config
+  websocket.value = new WebSocket('ws://localhost:3000/api/websocket')
+
+  websocketConnectionState.value = websocket.value.readyState
+
+  websocket.value.addEventListener('open', () => {
+    websocketConnectionState.value = websocket.value?.readyState
+  })
+  websocket.value.addEventListener('close', () => {
+    websocketConnectionState.value = websocket.value?.readyState
+  })
+  websocket.value.addEventListener('error', () => {
+    websocketConnectionState.value = websocket.value?.readyState
+  })
+}
+
 export function useWebsocket() {
-  const websocket = ref<WebSocket | null>(null)
-  const websocketConnectionState = ref<WebSocket['readyState'] | undefined>(undefined)
-  const websocketConnectionStateString = computed(() =>
-    getWebsocketStateString(websocketConnectionState.value),
-  )
-
-  const isWebsocketConnected = computed(() => websocketConnectionState.value === WebSocket.OPEN)
-
-  function setupWebsocket() {
-    // @todo Move the URL into a config
-    websocket.value = new WebSocket('ws://localhost:3000/api/websocket')
-
-    websocketConnectionState.value = websocket.value.readyState
-
-    websocket.value.addEventListener('open', () => {
-      websocketConnectionState.value = websocket.value?.readyState
-    })
-    websocket.value.addEventListener('close', () => {
-      websocketConnectionState.value = websocket.value?.readyState
-    })
-    websocket.value.addEventListener('error', () => {
-      websocketConnectionState.value = websocket.value?.readyState
-    })
-  }
-
   return {
     setupWebsocket,
     websocket,
