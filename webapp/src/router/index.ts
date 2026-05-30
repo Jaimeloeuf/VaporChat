9e25fd9dd49f15d736453b38e5113f6d994986e6
Jaime <jaimeloeuf@gmail.com>
2026-05-31 01:39:30 +0800

    Make ChatStart and Chat component into routes.

--- webapp/src/router/index.ts
+++ webapp/src/router/index.ts
@@ -2,7 +2,19 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'chat-start',
+      component: () => import('../ChatStart.vue'),
+    },
+    {
+      // @todo Maybe room ID should be in this link?
+      path: '/chat/room',
+      name: 'chat-room',
+      component: () => import('../Chat.vue'),
+    },
+  ],
 })
 
 export default router
