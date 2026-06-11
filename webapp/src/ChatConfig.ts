371b439ffe41a20d7fb57c20cfacc5d3dde36c3c
Jaime <jaimeloeuf@gmail.com>
2026-06-11 19:28:42 +0800

    Write ChatConfig as zod schema.

--- webapp/src/ChatConfig.ts
+++ webapp/src/ChatConfig.ts
@@ -1,6 +1,10 @@
-export type ChatConfig = {
-  chatRoomTTL: number
-  maxNumberOfParticipants: number
-  maxHistoryDurationInSeconds: number
-  maxMessagesLength: number
-}
+import { z } from 'zod'
+
+export const ChatConfigSchema = z.object({
+  chatRoomTTL: z.number().positive(),
+  maxNumberOfParticipants: z.number().positive(),
+  maxHistoryDurationInSeconds: z.number().positive(),
+  maxMessagesLength: z.number().positive(),
+})
+
+export type ChatConfig = z.infer<typeof ChatConfigSchema>
