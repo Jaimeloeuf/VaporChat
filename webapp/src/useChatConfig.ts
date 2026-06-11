ac5c30c3a7029a01e553ca545391f3ee664f0368
Jaime <jaimeloeuf@gmail.com>
2026-06-11 19:29:44 +0800

    Rewrite default chatConfig with zod schema.

--- webapp/src/useChatConfig.ts
+++ webapp/src/useChatConfig.ts
@@ -1,12 +1,14 @@
 import { reactive, readonly } from 'vue'
-import type { ChatConfig } from './ChatConfig'
+import { ChatConfigSchema } from './ChatConfig'
 
-const chatConfig = reactive<ChatConfig>({
-  chatRoomTTL: 300,
-  maxNumberOfParticipants: 2,
-  maxHistoryDurationInSeconds: 120,
-  maxMessagesLength: 20,
-})
+const chatConfig = reactive(
+  ChatConfigSchema.parse({
+    chatRoomTTL: 300,
+    maxNumberOfParticipants: 2,
+    maxHistoryDurationInSeconds: 120,
+    maxMessagesLength: 20,
+  }),
+)
 
 const readonlyChatConfig = readonly(chatConfig)
 
